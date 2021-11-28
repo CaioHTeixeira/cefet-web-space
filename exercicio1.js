1737a51d76f32e8e9d379963558b221aab9c53bf
CaioHTeixeira <caioteixeiraaa@gmail.com>
2021-11-28 19:37:24 -0300

    exe 1

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,17 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+let inputResultado = document.querySelector('#resultado');
+const btnCalcular = document.querySelector('#calcular');
+const inputConstante = document.querySelector('#constante');
+const inputMassa1 = document.querySelector('#massa1');
+const inputMassa2 = document.querySelector('#massa2');
+const inputDistancia = document.querySelector('#distancia');
+
+// console.log(inputConstante.value);
+btnCalcular.addEventListener('click', () => {
+    let resultado = (inputConstante.value * inputMassa1.value 
+        * inputMassa2.value)/(Math.pow(inputDistancia.value, 2));
+    
+    inputResultado.value = resultado;
+})
